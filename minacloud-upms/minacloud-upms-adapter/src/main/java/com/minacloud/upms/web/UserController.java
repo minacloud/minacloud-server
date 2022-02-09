3a368b22a7914278d385af3567e4651e0435a511
lishan <lslvxy@163.com>
2022-02-09 11:55:26 +0800

    init commit

--- minacloud-upms/minacloud-upms-adapter/src/main/java/com/minacloud/upms/web/UserController.java
+++ minacloud-upms/minacloud-upms-adapter/src/main/java/com/minacloud/upms/web/UserController.java
@@ -43,6 +43,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -51,7 +52,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/user")
-    public Response createUser(UserAddCmd cmd) {
+    public Response createUser(@RequestBody UserAddCmd cmd) {
         return userService.createUser(cmd);
     }
 
